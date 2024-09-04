254693f687ad5782cef5a71d825167f2a71fbeb1
Mohamed Eid <mohamed.eid.flutter@gmail.com>
2024-09-04 18:40:28 +0300

    add fetch all notes cubit

--- lib/features/notes/presentation/cubit/read_notes_cubit/read_notes_cubit.dart
+++ lib/features/notes/presentation/cubit/read_notes_cubit/read_notes_cubit.dart
@@ -1,9 +1,22 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/features/notes/data/models/note_model.dart';
+
+import '../../../../../core/theme/constants.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  fetchAllNotes() {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = noteBox.values.toList();
+      emit(ReadNotesSuccess(notes: notes));
+    } catch (e) {
+      emit(ReadNotesFailure(errMessage: e.toString()));
+    }
+  }
 }
--- lib/features/notes/presentation/pages/home_page.dart
+++ lib/features/notes/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/features/notes/presentation/cubit/read_notes_cubit/read_notes_cubit.dart';
 
 import '../widgets/add_note_bottom_sheet.dart';
 import '../widgets/home_page_body_widget.dart';
@@ -7,20 +9,23 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const HomePageBodyWidget(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            builder: (context) => const AddNoteBottomSheet(),
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: Scaffold(
+        body: const HomePageBodyWidget(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              builder: (context) => const AddNoteBottomSheet(),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
